420de7602016b77d023019b6bb670f8d221aefd3
Pham Thanh Tu <tu@hottab.net>
2020-09-08 11:03:44 +0700

    initial commit

--- covid-mask-detector/train.py
+++ covid-mask-detector/train.py
@@ -18,53 +18,54 @@ from torch.optim import Adam
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import DataLoader
 
-from .dataset import MaskDataset
+from dataset import MaskDataset
 
 
 class MaskDetector(pl.LightningModule):
     """ MaskDetector PyTorch Lightning class
     """
-    def __init__(self, maskDFPath: Path=None):
+
+    def __init__(self, maskDFPath: Path = None):
         super(MaskDetector, self).__init__()
         self.maskDFPath = maskDFPath
-        
+
         self.maskDF = None
         self.trainDF = None
         self.validateDF = None
         self.crossEntropyLoss = None
         self.learningRate = 0.00001
-        
+
         self.convLayer1 = convLayer1 = Sequential(
             Conv2d(3, 32, kernel_size=(3, 3), padding=(1, 1)),
             ReLU(),
             MaxPool2d(kernel_size=(2, 2))
         )
-        
+
         self.convLayer2 = convLayer2 = Sequential(
             Conv2d(32, 64, kernel_size=(3, 3), padding=(1, 1)),
             ReLU(),
             MaxPool2d(kernel_size=(2, 2))
         )
-        
+
         self.convLayer3 = convLayer3 = Sequential(
-            Conv2d(64, 128, kernel_size=(3, 3), padding=(1, 1), stride=(3,3)),
+            Conv2d(64, 128, kernel_size=(3, 3), padding=(1, 1), stride=(3, 3)),
             ReLU(),
             MaxPool2d(kernel_size=(2, 2))
         )
-        
+
         self.linearLayers = linearLayers = Sequential(
             Linear(in_features=2048, out_features=1024),
             ReLU(),
             Linear(in_features=1024, out_features=2),
         )
-        
+
         # Initialize layers' weights
         for sequential in [convLayer1, convLayer2, convLayer3, linearLayers]:
             for layer in sequential.children():
                 if isinstance(layer, (Linear, Conv2d)):
                     init.xavier_uniform_(layer.weight)
-    
-    def forward(self, x: Tensor): # pylint: disable=arguments-differ
+
+    def forward(self, x: Tensor):  # pylint: disable=arguments-differ
         """ forward pass
         """
         out = self.convLayer1(x)
@@ -73,61 +74,63 @@ class MaskDetector(pl.LightningModule):
         out = out.view(-1, 2048)
         out = self.linearLayers(out)
         return out
-    
+
     def prepare_data(self) -> None:
         self.maskDF = maskDF = pd.read_pickle(self.maskDFPath)
         train, validate = train_test_split(maskDF, test_size=0.3, random_state=0,
                                            stratify=maskDF['mask'])
         self.trainDF = MaskDataset(train)
         self.validateDF = MaskDataset(validate)
-        
+
         # Create weight vector for CrossEntropyLoss
-        maskNum = maskDF[maskDF['mask']==1].shape[0]
-        nonMaskNum = maskDF[maskDF['mask']==0].shape[0]
+        maskNum = maskDF[maskDF['mask'] == 1].shape[0]
+        nonMaskNum = maskDF[maskDF['mask'] == 0].shape[0]
         nSamples = [nonMaskNum, maskNum]
         normedWeights = [1 - (x / sum(nSamples)) for x in nSamples]
-        self.crossEntropyLoss = CrossEntropyLoss(weight=torch.tensor(normedWeights))
-    
+        self.crossEntropyLoss = CrossEntropyLoss(
+            weight=torch.tensor(normedWeights))
+
     def train_dataloader(self) -> DataLoader:
         return DataLoader(self.trainDF, batch_size=32, shuffle=True, num_workers=4)
-    
+
     def val_dataloader(self) -> DataLoader:
         return DataLoader(self.validateDF, batch_size=32, num_workers=4)
-    
+
     def configure_optimizers(self) -> Optimizer:
         return Adam(self.parameters(), lr=self.learningRate)
-    
-    def training_step(self, batch: dict, _batch_idx: int) -> Dict[str, Tensor]: # pylint: disable=arguments-differ
+
+    def training_step(self, batch: dict, _batch_idx: int) -> Dict[str, Tensor]:  # pylint: disable=arguments-differ
         inputs, labels = batch['image'], batch['mask']
         labels = labels.flatten()
         outputs = self.forward(inputs)
         loss = self.crossEntropyLoss(outputs, labels)
-        
+
         tensorboardLogs = {'train_loss': loss}
         return {'loss': loss, 'log': tensorboardLogs}
-    
-    def validation_step(self, batch: dict, _batch_idx: int) -> Dict[str, Tensor]: # pylint: disable=arguments-differ
+
+    def validation_step(self, batch: dict, _batch_idx: int) -> Dict[str, Tensor]:  # pylint: disable=arguments-differ
         inputs, labels = batch['image'], batch['mask']
         labels = labels.flatten()
         outputs = self.forward(inputs)
         loss = self.crossEntropyLoss(outputs, labels)
-        
+
         _, outputs = torch.max(outputs, dim=1)
         valAcc = accuracy_score(outputs.cpu(), labels.cpu())
         valAcc = torch.tensor(valAcc)
-        
-        return {'val_loss': loss, 'val_acc':valAcc}
-    
+
+        return {'val_loss': loss, 'val_acc': valAcc}
+
     def validation_epoch_end(self, outputs: List[Dict[str, Tensor]]) \
             -> Dict[str, Union[Tensor, Dict[str, Tensor]]]:
         avgLoss = torch.stack([x['val_loss'] for x in outputs]).mean()
         avgAcc = torch.stack([x['val_acc'] for x in outputs]).mean()
-        tensorboardLogs = {'val_loss': avgLoss, 'val_acc':avgAcc}
+        tensorboardLogs = {'val_loss': avgLoss, 'val_acc': avgAcc}
         return {'val_loss': avgLoss, 'log': tensorboardLogs}
+
 
 if __name__ == '__main__':
     model = MaskDetector(Path('covid-mask-detector/data/mask_df.pickle'))
-    
+
     checkpoint_callback = ModelCheckpoint(
         filepath='covid-mask-detector/checkpoints/weights.ckpt',
         save_weights_only=True,
--- covid-mask-detector/video.py
+++ covid-mask-detector/video.py
@@ -7,73 +7,79 @@ import cv2
 import torch
 from skvideo.io import FFmpegWriter, vreader
 from torchvision.transforms import Compose, Resize, ToPILImage, ToTensor
-
-from .common.facedetector import FaceDetector
-from .train import MaskDetector
+import os
+from common.facedetector import FaceDetector
+from train import MaskDetector
 
 
 @click.command(help="""
                     modelPath: path to model.ckpt\n
                     videoPath: path to video file to annotate
                     """)
-@click.argument('modelpath')
-@click.argument('videopath')
+@click.option('--model', 'modelPath', default="models")
+@click.option('--input', 'videoPath', type=Path, default=None)
 @click.option('--output', 'outputPath', type=Path,
               help='specify output path to save video with annotations')
 @torch.no_grad()
-def tagVideo(modelpath, videopath, outputPath=None):
+def tagVideo(modelPath, videoPath=None, outputPath=None):
     """ detect if persons in video are wearing masks or not
     """
     model = MaskDetector()
-    model.load_state_dict(torch.load(modelpath)['state_dict'], strict=False)
-    
+    model.load_state_dict(torch.load(os.path.join(modelPath, 'face_mask.ckpt'), map_location=torch.device('cpu'))[
+                          'state_dict'], strict=False)
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     model.eval()
-    
+
     faceDetector = FaceDetector(
-        prototype='covid-mask-detector/models/deploy.prototxt.txt',
-        model='covid-mask-detector/models/res10_300x300_ssd_iter_140000.caffemodel',
+        prototype=os.path.join(modelPath, 'deploy.prototxt.txt'),
+        model=os.path.join(
+            modelPath, 'res10_300x300_ssd_iter_140000.caffemodel'),
     )
-    
+
     transformations = Compose([
         ToPILImage(),
         Resize((100, 100)),
         ToTensor(),
     ])
-    
+
     if outputPath:
         writer = FFmpegWriter(str(outputPath))
-    
+
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.namedWindow('main', cv2.WINDOW_NORMAL)
     labels = ['No mask', 'Mask']
     labelColor = [(10, 0, 255), (10, 255, 0)]
-    for frame in vreader(str(videopath)):
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    cap = cv2.VideoCapture(0 if videoPath is None else videoPath)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+    # for frame in vreader(str(videoPath)):
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         faces = faceDetector.detect(frame)
         for face in faces:
             xStart, yStart, width, height = face
-            
+
             # clamp coordinates that are outside of the image
             xStart, yStart = max(xStart, 0), max(yStart, 0)
-            
+
             # predict mask label on extracted face
             faceImg = frame[yStart:yStart+height, xStart:xStart+width]
             output = model(transformations(faceImg).unsqueeze(0).to(device))
             _, predicted = torch.max(output.data, 1)
-            
+
             # draw face frame
             cv2.rectangle(frame,
                           (xStart, yStart),
                           (xStart + width, yStart + height),
                           (126, 65, 64),
                           thickness=2)
-            
+
             # center text according to the face frame
             textSize = cv2.getTextSize(labels[predicted], font, 1, 2)[0]
             textX = xStart + width // 2 - textSize[0] // 2
-            
+
             # draw prediction label
             cv2.putText(frame,
                         labels[predicted],
@@ -87,6 +93,7 @@ def tagVideo(modelpath, videopath, outputPath=None):
     if outputPath:
         writer.close()
     cv2.destroyAllWindows()
+
 
 # pylint: disable=no-value-for-parameter
 if __name__ == '__main__':
